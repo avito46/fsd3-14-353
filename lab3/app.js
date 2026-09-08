8d01b4e91225677ec485f42f204d9712c5463a63
Aviral Dwivedi <aviraldwivedi410@gmail.com>
2026-09-08 10:18:27 +0530

    updated

--- lab3/app.js
+++ lab3/app.js
@@ -1,16 +1,66 @@
 import http from 'http'
+import { parse as parseUrl } from 'url'
+import { getAllTeams } from './teams.js'
 
-/*const server=http.createServer()
-server.on('request',(req,res)=>{
-    res.write("<h1>Welcome to server side programming</h1>")
-    res.write("Nodemon is tracking files")
-    res.end()
-})*/
+const port = 5000
 
-const server=http.createServer((req,res)=>{
-    res.end("<h1>Welcome to server side</h1>")
+const sendJSON = (res, statusCode, data) => {
+
+    res.writeHead(statusCode, {
+        "content-type": "application/json"
+    })
+
+    res.end(data === undefined ? "" : JSON.stringify(data))
+}
+
+const parseJSONBody = (req) => {
+
+    return new Promise((resolve, reject) => {
+
+        let body = ""
+
+        req.on("data", (chunk) => {
+            body += chunk.toString()
+        })
+
+        req.on("end", () => {
+
+            try {
+                resolve(body ? JSON.parse(body) : {})
+            }
+            catch (error) {
+                reject(error)
+            }
+
+        })
+
+    })
+}
+
+const server = http.createServer((req, res) => {
+
+    const { pathname, query } = parseUrl(req.url, true)
+
+    const method = req.method
+
+    console.log('pathname:', pathname)
+    console.log('query:', query)
+    console.log('Method:', method)
+
+    if (pathname === '/api/v1/teams' && method === 'GET') {
+
+        const teams = getAllTeams()
+
+        const { total } = query
+
+        return sendJSON(res, 200, teams)
+    }
+
+    return sendJSON(res, 404, {
+        message: "Route not found"
+    })
 })
 
-server.listen(5000,()=>{
-    console.log("Server is running")
+server.listen(port, () => {
+    console.log("Server is running at", port)
 })
