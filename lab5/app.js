21c2c04c8b66f9b5f61c6b32e91b1d3312071cde
Aviral Dwivedi <aviraldwivedi410@gmail.com>
2026-09-17 14:35:12 +0530

    sending html file to client

--- lab5/app.js
+++ lab5/app.js
@@ -1,7 +1,23 @@
-import express from 'express'
-const app=express()
-app.get("/",(req,res)=>{
-    res.send("<h1>hello express</h1>")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello Express</h1>");
+});
+app.get("/about",(req,res)=>{
+    res.send("WE ARE FSD DEVS")
 })
-app.use("*")
-app.listen(3000,()=>console.log("Server is running"))
+app.post("/login",(req,res)=>{
+    res.send({msg:'user login'})
+})
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+app.use((req,res)=>{
+    res.status(404).send("NOT FOUND")
+})
+app.listen(3333,()=>console.log("Server is running"))
---
+++ lab5/prg1.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { fileURLToPath } from 'node:url'
+import path from 'path'
+const port=3333
+const app=express()
+const filename=fileURLToPath(import.meta.url)
+const dirname=path.dirname(filename)
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(dirname,"public","index.html"))
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(dirname,"public","about.html"))
+})
+app.get("/enquiry",(req,res)=>{
+    res.sendFile(path.join(dirname,"public","enquiry.html"))
+})
+app.listen(port,()=>console.log("prg1 is running at",port))
